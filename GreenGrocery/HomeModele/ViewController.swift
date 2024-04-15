be192a416ede8806474f5da05c5247f59ef3159f
Enmanuel Torres <eenmanuel.jose.17.1@gmail.com>
2024-04-15 10:57:22 -0500

    Getting Started

---
+++ GreenGrocery/HomeModele/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GreenGrocery
+//
+//  Created by ENMANUEL TORRES on 15/04/24.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .blue
+    }
+
+
+}
+
